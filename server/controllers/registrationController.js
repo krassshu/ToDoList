0e27c66e0e4aec3c2361b031996da18033221d46
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-06 12:03:22 +0200

    created registration controller

---
+++ server/controllers/registrationController.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcrypt")
+const User = require("../models")
+
+const registration = async (req, res) => {
+	const { username, password, email } = req.body
+
+	let user = await User.findOne({ username })
+	if (user) return res.status(400).send("That user is already register.")
+
+	let mail = await User.findOne({ email })
+	if (mail) return res.status(400).send("This e-mail is already used.")
+
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(password, salt)
+
+	user = new User({ username, email, password: hashedPassword })
+	await user.save()
+	res.send({
+		message: "Registration succesful",
+		username: user.username,
+		email: user.email,
+	})
+}
+
+module.exports = { registration }
