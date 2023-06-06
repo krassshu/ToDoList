90479b7935fdb72b0dc0133535eb609da6f0700d
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-06 13:07:32 +0200

    token checker created

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,32 +2,30 @@ const jwt = require("jsonwebtoken")
 const { User } = require("../models")
 
 const login = async (req, res) => {
-    const { username, password } = req.body
-    const user = await User.findOne({ username })
-    if (!user) return res.status(400).send("This user does not exist.")
+	const { username, password } = req.body
+	const user = await User.findOne({ username })
+	if (!user) return res.status(400).send("This user does not exist.")
 
-    const validPassword = await User.findOne({ password })
-    if (!validPassword) return res.status(400).send("This password is invalid.")
+	const validPassword = await User.findOne({ password })
+	if (!validPassword) return res.status(400).send("This password is invalid.")
 
-    const token = jwt.sign(
-        {
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-        },
-        process.env.JWT_PRIVATE_KEY,
-        { expiresIn: "7d" }
-    )
+	const token = jwt.sign(
+		{
+			_id: user._id,
+			username: user.username,
+			email: user.email,
+		},
+		process.env.JWT_PRIVATE_KEY,
+		{ expiresIn: "7d" }
+	)
 
-    const userId = user._id.slice(0, -4)
-
-    res.cookie("userId", userId, {
-        httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
-    res.send({
-        message: "Login successful",
-    })
+	res.cookie("userId", token, {
+		httpOnly: true,
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	})
+	res.send({
+		message: "Login successful",
+	})
 }
 
 module.exports = { login }
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken")
+
+function auth(req, res, next) {
+	const token = req.cookie.userId
+	if (!token) return res.status(401).send("Access denide. No token provided.")
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY)
+		req.user = decoded
+		next()
+	} catch (error) {
+		res.status(400).send("Invalid token.")
+	}
+}
+
+module.exports = { auth }
