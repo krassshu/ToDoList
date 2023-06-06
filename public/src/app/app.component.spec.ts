1a9edfd96699d92d096597a02270f9c8ac3db654
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-06 15:35:59 +0200

    start working on front

--- public/src/app/app.component.spec.ts
+++ public/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todoList'`, () => {
+  it(`should have as title 'public'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todoList');
+    expect(app.title).toEqual('public');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todoList app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('public app is running!');
   });
 });
