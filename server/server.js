4b37a478d2c26b35a5113fb4fa180244e0f4bff1
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-06-06 13:18:40 +0200

    added routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
 
-
+const loginRoutes = require("./router/loginRoutes")
+const registrationRoutes = require("./router/registrationRoutes")
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
 
+app.use("/", loginRoutes)
+app.use("/", registrationRoutes)
 
 mongoose
 	.connect(process.env.MONGODB_URI, {
@@ -14,7 +20,6 @@ mongoose
 	})
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Failed to connect to MongoDB", err))
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
